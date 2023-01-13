79086a307ed6e02cefd8a1fd82070c94714a63bc
jaydeegabrillo <68362401+jaydeegabrillo@users.noreply.github.com>
2023-01-13 11:34:41 +0800

    create login filter

---
+++ app/Filters/MyFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+
+class MyFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null) {
+        $logged_in = session()->get('logged_in');
+        $login_uri = explode('?', $_SERVER['REQUEST_URI']);
+        
+        if(!isset($logged_in)){
+            if($login_uri[0] != '/login'){
+                if($login_uri[0] != '/login/login'){
+                    return redirect()->to(base_url());
+                }
+            }
+        }else{
+            if($login_uri[0] == '/login'){
+                return redirect()->to(base_url());
+            }
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+        // Do something here
+    }
+}
