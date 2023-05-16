5155256f8b6ee268bd5bfdb0a2edf1985ce2cccd
jaydeegabrillo <68362401+jaydeegabrillo@users.noreply.github.com>
2023-05-16 09:33:16 +0800

    Update sidebar.php

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
     <a href="index3.html" class="brand-link">
-        <img src="assets/images/delsanva.jpg" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8">
+        <img src="assets/images/delsanva.jpg" alt="DelsanVA Logo" class="brand-image elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Delsan VA</span>
     </a>
 
@@ -62,7 +62,7 @@
 <div class="wrapper">
 
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="assets/images/delsanva.jpg" alt="DelsanVA Logo" style="height:100; min-width:100; width:100%;">
     </div>
 
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
